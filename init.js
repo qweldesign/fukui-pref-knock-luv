232e0020e66d85962895f65fd2ca5f9a1f4314cf
QWEL.DESIGN <webmaster@qwel.design>
2025-08-20 14:39:57 +0900

    Create an init.js to centrally manage UI components
    Insert a banner and reveal it by scrolling

---
+++ init.js
@@ -0,0 +1,15 @@
+/**
+ * Init
+ * このファイルは QWEL Project の一部です。
+ * Part of the QWEL Project © QWEL.DESIGN 2025
+ * Licensed under GPL v3 – see https://qwel.design/
+ */
+
+import PersonalityTest from './js/personalityTest.js';
+new PersonalityTest();
+
+import PersonalityResult from './js/personalityResult.js';
+new PersonalityResult();
+
+import RevealOnScroll from './js/revealOnScroll.js';
+new RevealOnScroll();
