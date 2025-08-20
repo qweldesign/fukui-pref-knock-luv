ba04cd33ae38300e0233838e365447430a827f3b
QWEL.DESIGN <webmaster@qwel.design>
2025-08-20 13:19:49 +0900

    Work in progress (personality test results page)

--- js/personalityResult.js
+++ js/personalityResult.js
@@ -4,6 +4,23 @@
 
 export default class PersonalityResult {
   constructor() {
-    
+    this.elem = document.getElementById('result');
+    if (!this.elem) return;
+
+    const hash = location.hash;
+    const types = ['efa', 'efp', 'eta', 'etp', 'ifa', 'ifp', 'ita', 'itp'];
+    const type = hash.slice(1);
+    if (types.includes(type)) {
+      const template = document.getElementById(`type_${type}`);
+      const clone = template.content.cloneNode(true);
+      this.elem.appendChild(clone);
+    } else {
+      location.href = '../';
+    }
+
+    window.addEventListener('hashchange', () => {
+      window.scrollTo(0, 0);
+      location.reload();
+    });
   }
 }
